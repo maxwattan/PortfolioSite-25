deb2919d1496b7d522359c00ddeb403d264aff83
MaxWattan <mcwattanac@gmail.com>
2025-04-29 20:55:38 -0400

    made project section might change if more time

--- Portfolio25/src/App.jsx
+++ Portfolio25/src/App.jsx
@@ -1,5 +1,5 @@
 // import logo from "./assets/mwlogo.png"
-import meditate from "./assets/Meditate.jpg"
+// import meditate from "./assets/Meditate.jpg"
 import me from "./assets/profile.jpg"
 import projectOne from "./assets/projectOne.png"
 import projecttwo from "./assets/projectTwo.png";
@@ -40,7 +40,7 @@ function App() {
           <h1> Welcome </h1>
           <h2>Take a Deep Breath...</h2>
           <h3> and Exhale...</h3>
-          <img src={meditate} alt="meditate" className="meditate"></img>
+          {/* <img src={meditate} alt="meditate" className="meditate"></img> */}
         </div>
       </section>
 
@@ -79,59 +79,72 @@ function App() {
           </div>
 
           <div className="background-content">
+            <h2 className="background-title"> Background </h2>
 
-          <h2 className="background-title"> Background </h2>
+            <p className="background-description">
+              Before entering the tech world, I pursued a career in hospitality
+              with my Bachelor's in Hospitality Management. Due to physical
+              injuries, I found it challenging to physically keep up with the
+              fast-paced enviornment. This led me to transition my skills into
+              tech. As a full stack web developer, I am passionate about
+              creating platforms that enhance our lives, benefit our communities
+              and support our environment. I bring a unique perspective and a
+              commitment to making a positive impact through technology.
+            </p>
 
-          <p className="background-description">
-            Before entering the tech world, I pursued a career in hospitality
-            with my Bachelor's in Hospitality Management. Due to physical
-            injuries, I found it challenging to physically keep up with the
-            fast-paced enviornment. This led me to transition my skills into
-            tech. As a full stack web developer, I am passionate about creating
-            platforms that enhance our lives, benefit our communities and
-            support our environment. I bring a unique perspective and a
-            commitment to making a positive impact through technology.
-          </p>
+            <p className="hobbies">
+              Outside of coding some of my hobbies includes: cooking, baking,
+              fishing, and exercising.
+            </p>
 
-          <p className="hobbies">
-            Outside of coding some of my hobbies includes: cooking, baking,
-            fishing, and exercising.
-          </p>
-
-          <p className="fun-fact">
-            Fun Fact About Me: I was a monk twice in my life.
-          </p>
+            <p className="fun-fact">
+              Fun Fact About Me: I was a monk twice in my life.
+            </p>
           </div>
-
         </div>
       </section>
 
       <section className="projects" id="projects">
-        <h1 className="projects" id="projects">
+        <h1 className="projects-title" id="projects">
           Projects
         </h1>
-        <img
-          src={projectOne}
-          alt="projectOne"
-          className="projectOne"
-          height="500"
-          width="500"
-        ></img>
-        <p>
-          Projects – A placeholder or sample project card (you'll add more here
-          throughout the program)
+
+        <div className="project-card">
+        <img src={projectOne} alt="projectOne" ></img>
+        <div className="project-content">
+        <p className="project-description">
+          This is the first app I created when I first learned to code. It’s a
+          recipe finder that suggests kitchen food ideas along with a list of
+          ingredients and cooking instructions. The app integrates an API and
+          includes some basic CSS styling. It’s a great reminder of how I got
+          started and how far I have come.
         </p>
-        <img
-          src={projecttwo}
-          alt="projectTwo"
-          className="projectTwo"
-          height="500"
-          width="500"
-        ></img>
-        <p>
-          Projects – A placeholder or sample project card (you'll add more here
-          throughout the program)
+        <button className="project-button">
+          <a href="https://zippy-horse-6f8f0e.netlify.app/" className="recipe-finder"> Live Site! </a>
+        </button>
+        </div>
+        </div>
+
+        <div className="project-card">
+        <img src={projecttwo} alt="projectTwo"></img>
+        <div className="project-content">
+        <p className="project-description">
+          Care Village is a four-person capstone team with a mission to empower
+          parents and caregivers on their unique parenting journeys while
+          fostering a compassionate and supportive community. We provide a
+          platform that seamlessly connects individuals with diverse parenting
+          methods and offers a dedicated space for the generosity of giving to
+          children in need. We believe in the power of knowledge-sharing and
+          collaboration among parents, grandparents, guardians, and loved ones.
         </p>
+        <button className="project-button">
+          <a href="https://carevillage.netlify.app/" className="care-village">
+            Live Site!
+          </a>
+        </button>
+        </div>
+        </div>
+
       </section>
 
       <section className="contact" id="contact">
